b6cdb497bf23b61bf39be5c93b719317fd370f02
huyang <michael531@bupt.edu.cn>
2022-05-10 14:18:59 +0800

    minor refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,15 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
         return super().tearDown()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        # 繁！
+        # self.assertTrue(
+        #     any(row.text == '1. Buy peacock feathers' for row in rows),
+        #     f'New to-do item did not appear in table. Contents were:\n{table.text}')  # add comment because otherwise it is vague
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app. She goes
@@ -51,16 +60,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and shows both items on her list.
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2. Use peacock feathers to make a fly',
-                      [row.text for row in rows])
-        # 繁！
-        # self.assertTrue(
-        #     any(row.text == '1. Buy peacock feathers' for row in rows),
-        #     f'New to-do item did not appear in table. Contents were:\n{table.text}')  # add comment because otherwise it is vague
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2. Use peacock feathers to make a fly')
 
         self.fail('Test finished!')
 
