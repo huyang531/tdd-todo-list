7bc5dbbe16b9471259ad04c9e401c0b39966422c
huyang <michael531@bupt.edu.cn>
2022-05-10 14:11:53 +0800

    before refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,5 @@
+from urllib import response
+from matplotlib.pyplot import ticklabel_format
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
@@ -41,11 +43,24 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly". (Eidth is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and shows both items on her list.
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table')  # add comment because otherwise it is vague
+
+        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2. Use peacock feathers to make a fly',
+                      [row.text for row in rows])
+        # 繁！
+        # self.assertTrue(
+        #     any(row.text == '1. Buy peacock feathers' for row in rows),
+        #     f'New to-do item did not appear in table. Contents were:\n{table.text}')  # add comment because otherwise it is vague
 
         self.fail('Test finished!')
 
@@ -53,11 +68,6 @@ class NewVisitorTest(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
 
-
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly". (Eidth is very methodical)
-
-# The page updates again, and shows both items on her list.
 
 # Eidth wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her—there is some
