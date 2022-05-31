81e93058a26bf4ae3d8464cf8079e297b4ebc544
huyang <michael531@bupt.edu.cn>
2022-05-31 13:14:57 +0800

    new URL, view and tempalte to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -32,12 +32,8 @@ class SomkeTest(TestCase):
     def test_redirect_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-                         '/lists/the-only-list-in-the-world')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-        # test redirect
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
 
     # def test_root_url_resolve_to_home_page_view(self):
     #     found = resolve('/')
@@ -89,25 +85,13 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_lists_items(self):
-        # setup
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        # exercise
-        response = self.client.get('/')
-
-        # assert
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -8,12 +8,11 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    return render(request, 'home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
-
-    # 骗！
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'list.html', {'items': items})
